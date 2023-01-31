2c1d471f72060c17fc855223589844ed8f594c68
Nikita <40544749+Elfilagas@users.noreply.github.com>
2023-01-31 08:48:43 +0300

    Update SELECT_DATA.sql
    Format SELECT_DATA.sql

--- SELECT_DATA.sql
+++ SELECT_DATA.sql
@@ -1,17 +1,30 @@
 -- название и год выхода альбомов, вышедших в 2018 году
-SELECT album_name, release_year FROM albums WHERE release_year = 2018;
+SELECT album_name, release_year 
+  FROM albums 
+ WHERE release_year = 2018;
 
 -- название и продолжительность самого длительного трека
-SELECT track_name, duration FROM tracks ORDER BY duration DESC LIMIT 1;
+SELECT track_name, duration 
+  FROM tracks 
+ ORDER BY duration DESC 
+ LIMIT 1;
 
 -- название треков, продолжительность которых не менее 3,5 минуты
-SELECT track_name FROM tracks WHERE duration >= 60 * 3.5;
+SELECT track_name 
+  FROM tracks 
+ WHERE duration >= 60 * 3.5;
 
 -- названия сборников, вышедших в период с 2018 по 2020 год включительно
-SELECT collection_name FROM collections WHERE release_year BETWEEN 2018 AND 2020;
+SELECT collection_name 
+  FROM collections 
+ WHERE release_year BETWEEN 2018 AND 2020;
 
 -- исполнители, чье имя состоит из 1 слова
-SELECT artist_name FROM artists WHERE artist_name NOT LIKE '% %';
+SELECT artist_name 
+  FROM artists 
+ WHERE artist_name NOT LIKE '% %';
 
 -- название треков, которые содержат слово "мой"/"my"
-SELECT track_name FROM tracks WHERE LOWER(track_name) LIKE '%my%';
+SELECT track_name 
+  FROM tracks 
+ WHERE LOWER(track_name) LIKE '%my%';
